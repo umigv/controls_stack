e9b62ccec5398e54ac9f2ca2998405af15f8d5b4
Ben Rossano <brossano@umich.edu>
2022-10-27 16:01:48 -0700

    initial commit

---
+++ src/rpastar.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <vector>
+#include "rpastar.h"
+#include "cmath"
+
+static const int threshold = 50;
+
+//default constructor
+rpastar::Node::Node() : g_score(INFINITY), h_score(0) {}
+
+//main constructor used for creating a Node
+
+//constructor for the start node which sets rhs to 0
+rpastar::Node::Node(int row, int col, Node * parent) : parent(parent) {
+    state = {row,col};
+    g_score = parent->g_score + 1;
+}
+
+void rpastar::Node::set_h(std::pair<int,int> &target_state)
+{
+    h_score = sqrt(pow(target_state.first - state.first, 2) - pow(target_state.second - state.second, 2));
+}
+
+void rpastar::Node::set_g(float g_in)
+{
+    g_score = g_in;
+}
+
+void rpastar::Node::set_state(int row, int col)
+{
+    state.first = row;
+    state.second = col;
+}
+
+void rpastar::Node::set_parent(Node *parent_in)
+{
+    parent = parent_in;
+}
+
+std::pair<int,int> rpastar::Node::get_state()
+{
+    return state;
+}
+
+float rpastar::Node::get_f_score() const
+{
+    return g_score + h_score;
+}
+
+bool rpastar::Node::at_target(std::pair<int,int> &target_state)
+{
+    return (state.first == target_state.first && state.second == target_state.second);
+}
+
+
+rpastar::rpastar(std::pair<int,int> start_state_in, std::pair<int,int> target_state_in) : start_state(start_state_in), target_state(target_state_in)
+{
+    //these are in the a* old implementation but don't think we need them as they are called in subscribe in listener.cpp
+    // gpsCall();
+    // costMapCallback(); 
+
+}
+
+void rpastar::find_target()
+{
+
+}
+
+void rpastar::search()
+{
+    while (!U.empty())
+    {
+        Node current_node = U.top();
+        U.pop();
+        open_set.erase(current_node.get_state());
+        if (current_node.at_target(target_state))
+        {
+            break;
+        }
+        int i = current_node.get_state().first;
+        int j = current_node.get_state().second;
+        processNode(i-1, j, &current_node); // north
+        processNode(i, j+1, &current_node); // east
+        processNode(i+1, j, &current_node); // south
+        processNode(i, j-1, &current_node); // west
+        processNode(i-1, j+1, &current_node); // north-east
+        processNode(i+1, j+1, &current_node);// south-east
+        processNode(i-1, j-1, &current_node); // north-west
+        processNode(i+1, j+1, &current_node); // south-west
+        closed_set[current_node.get_state()] = current_node.get_f_score();
+    }
+}
+
+void rpastar::processNode(int row, int col, Node *parent)
+{
+    if (row < 0 || row >= cost_map.size() || col < 0 || col >= cost_map[0].size())
+    {
+        return;
+    }
+    if (cost_map[row][col] > threshold)
+    {
+        return;
+    }
+    Node &new_node = graph[row][col];
+    new_node = Node(row,col,parent);
+    new_node.set_h(target_state);
+    auto open_it = open_set.find(new_node.get_state());
+    if(open_it != open_set.end()){
+        if (open_it->second < new_node.get_f_score())
+        {
+            return;
+        }
+    }
+    auto closed_it = closed_set.find(new_node.get_state());
+    if(closed_it != closed_set.end()){
+        if (closed_it->second < new_node.get_f_score())
+        {
+            return;
+        }
+    }
+    U.push(new_node);
+    open_set[new_node.get_state()] = new_node.get_f_score();
+    return;
+}
+
+// called each time a new costmap is recieved...
+void rpastar::costMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+	// Fill out the costmap width and height from the occupancy grid info message
+    int costmap_width = msg->info.width;
+    int costmap_height = msg->info.height;
+    
+
+//initializing graph of nodes
+    // graph.clear();
+    // graph.resize(costmap_height);
+    // for (int i = 0; i < graph.size(); i++)
+    // {
+    //     graph[i].resize(costmap_width);
+    // }
+    find_target();
+    //graph[costmap_height/2][costmap_width/2].set_g(0);
+
+
+    // Fill out the costmap member variable using info from the occupancy grid costmap message
+    for(int i = 0; i < costmap_width; i++){
+        for(int j = 0; j < costmap_height; j++){
+            cost_map[i][j] = msg->data[i + (j * costmap_width)];
+            graph[i][j].set_state(i,j);
+        }
+    }
+
+    // Fill out the needed "start" position member variable using info from the costmap origin message
+    start_state = {costmap_height/2,costmap_width/2};
+    graph[start_state.first][start_state.second].set_g(0); 
+    graph[start_state.first][start_state.second].set_h(target_state); 
+    graph[start_state.first][start_state.second].set_parent(nullptr); 
+
+    //forgets about old priority queue and pushes new start node (current position) to priority queue so that it is not empty when starting the search
+    U = std::priority_queue<rpastar::Node, std::vector<rpastar::Node>, customGreater>();
+    U.push(graph[start_state.first][start_state.second]);
+    open_set[start_state] = U.top().get_f_score();
+
+    
+}
+
+// void rpastar::gpsCallback(const nav_msgs::Odometry::ConstPtr& msg){
+//     // Fill out the needed "target" position member variable using info from the odom message
+//     target_state = {msg->pose.pose.position.x, msg->pose.pose.position.y};
+// }
+
+void rpastar::backtracker()
+{
+    Node *curr_node = &graph[target_state.first][target_state.second];
+    while(curr_node != nullptr)
+    {
+        path.push_back(*curr_node);
+        curr_node = curr_node->get_parent();
+    }
+}
---
+++ src/rpastar.h
@@ -0,0 +1,85 @@
+#ifndef RPASTAR_H
+#define RPASTAR_H
+
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <cmath>
+#include <vector>
+
+class rpastar
+{
+    protected:
+        class Node
+        {
+        private:
+            // distance from start node to current node
+            float g_score;
+            // straight line distance to the target node
+            float h_score;
+            //g_score + h_score
+            std::pair<int, int> state;
+            Node *parent;
+        public:
+            friend class GlobalPlanner
+            Node();
+            Node(int row, int col,Node *parent);
+            void set_h(std::pair<int,int> &target_state);
+            void set_g(float g_in);
+            void set_state(int row, int col);
+            void set_parent(Node *parent_in);
+            Node *get_parent()
+            {
+                return parent;
+            }
+            std::pair<int,int> get_state();
+            float get_f_score() const;
+            bool at_target(std::pair<int,int> &target_state);
+            const bool& operator==(const Node &rhs)
+            {
+                return this->state == rhs.state;
+            }
+            
+        };
+
+        class customGreater {
+            public:
+            bool operator() (Node &lhs, Node &rhs)
+            {
+                return lhs.get_f_score() > rhs.get_f_score();
+            }
+        };
+
+        class Node_hash {
+            size_t operator()(const Node& node) const;
+        };
+
+        class Compare_coord{
+            bool operator()(const Node& lhs, const Node& rhs);
+        };
+
+    public:
+        rpastar(std::pair<int,int> start_state_in, std::pair<int,int> target_state_in);
+        void find_target();
+        void search();
+        void backtracker();
+        void processNode(int x, int y, Node* parent);
+        //void gpsCallback(const nav_msgs::Odometry::ConstPtr& msg);
+        void costMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+        double calculateEuclideanDistance(const Node& node1, const Node& node2);
+
+    private:
+        std::priority_queue<Node, std::vector<Node>, customGreater> U;
+        std::pair<int,int> start_state;
+        std::pair<int,int_fast64_t> target_state;
+        std::vector<Node> path;
+        std::unordered_map<std::pair<int,int>, float> open_set;
+        std::unordered_map<std::pair<int,int>, float> closed_set;
+        std::vector<std::vector<int>> cost_map;
+        std::vector<std::vector<Node>> graph;
+        
+};
+
+
+
+#endif
