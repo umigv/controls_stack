524c86cabc60a31d5c13737bb3629008ff145f84
Ben Rossano <brossano@umich.edu>
2022-12-01 15:23:07 -0800

    works with turtlebot

--- src/listener.cpp
+++ src/listener.cpp
@@ -34,27 +34,71 @@
 #include "nav_msgs/Odometry.h"
 #include "std_msgs/Header.h"
 #include "nav_msgs/MapMetaData.h"
+#include "nav_msgs/Path.h"
+#include "geometry_msgs/PoseStamped.h"
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include "rpastar.cpp"
 
 nav_msgs::OccupancyGrid process_array(const std_msgs::String::ConstPtr& msg);
 void chatterCallback(const std_msgs::String::ConstPtr& msg);
-void chatterCallbackTurtleBot(const nav_msgs::OccupancyGrid::ConstPtr& msg1, const nav_msgs::Odometry::ConstPtr& msg2);
-
-
+nav_msgs::Path generate_path(std::vector<std::pair<int,int>> path);
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
 
+class Listener
+{
+public:
+  void chatterCallbackTurtleBot(const nav_msgs::OccupancyGrid::ConstPtr& msg1, const nav_msgs::Odometry::ConstPtr& msg2, const geometry_msgs::PoseStamped::ConstPtr& msg3);
+  void generate_path(std::vector<std::pair<int,int>> path, const nav_msgs::OccupancyGrid::ConstPtr& map);
+  nav_msgs::Path get_path();
+  geometry_msgs::PoseStamped get_goal();
+
+private:
+  nav_msgs::Path nav_path;
+  geometry_msgs::PoseStamped goal_pose;
+};
+
+nav_msgs::Path Listener::get_path()
+{
+  return nav_path;
+}
+
+geometry_msgs::PoseStamped Listener::get_goal()
+{
+  return goal_pose;
+}
+
+void Listener::generate_path(std::vector<std::pair<int,int>> path, const nav_msgs::OccupancyGrid::ConstPtr& map)
+{
+  geometry_msgs::PoseStamped pose;
+  nav_path.header.frame_id = "map";
+  for (int i = 0; i < path.size(); i++)
+  {
+    float global_x = (path[i].first*map->info.resolution) + map->info.origin.position.x;
+    float global_y = (path[i].second*map->info.resolution) + map->info.origin.position.y;
+    pose.pose.position.x = global_y;
+    pose.pose.position.y = global_x;
+    pose.header.frame_id = "map";
+    nav_path.poses.push_back(pose);
+  }
+}
+
 
 // %Tag(CALLBACK)%
-void chatterCallbackTurtleBot(const nav_msgs::OccupancyGrid::ConstPtr& msg1, const nav_msgs::Odometry::ConstPtr& msg2)
+void Listener::chatterCallbackTurtleBot(const nav_msgs::OccupancyGrid::ConstPtr& msg1, const nav_msgs::Odometry::ConstPtr& msg2,
+                                        const geometry_msgs::PoseStamped::ConstPtr& msg3)
 { 
   nav_msgs::OccupancyGrid map = *msg1;
   nav_msgs::Odometry pos = *msg2;
+  goal_pose = *msg3;
   int pos_x = (int)((pos.pose.pose.position.x - map.info.origin.position.x) / map.info.resolution);
   int pos_y = (int)((pos.pose.pose.position.y - map.info.origin.position.y) / map.info.resolution);
+
+  int goal_x = (int)((goal_pose.pose.position.x - map.info.origin.position.x) / map.info.resolution);
+  int goal_y = (int)((goal_pose.pose.position.y - map.info.origin.position.y) / map.info.resolution);
   // for (int i = 0; i < map.info.height; i++)
   // {
   //   for (int j = 0; j < map.info.width; j++)
@@ -65,42 +109,44 @@ void chatterCallbackTurtleBot(const nav_msgs::OccupancyGrid::ConstPtr& msg1, con
   // }
   std::cout << pos_x << " " << pos_y << std::endl;
   std::cout << "Running A*..." << std::endl << std::endl;
-  std::pair<int,int> start(pos_x, pos_y);
-  std::pair<int,int> end(160, 250);
+  std::pair<int,int> start(pos_y, pos_x);
+  std::pair<int,int> end(goal_y, goal_x);
   // rpastar runner = rpastar::rpastar(start, end, &map);
   rpastar runner(start, end, &map);
   runner.search();
+  nav_msgs::Path nav_path;
   if (runner.goal_found())
   {
     std::vector<std::pair<int,int>> path = runner.backtracker();
     std::cout << "Path found!" << std::endl;
-    for (int i = 0; i < map.info.height; i++)
-    {
-      for (int j = 0; j < map.info.width; j++)
-      {
-        std::pair<int, int> pair(i,j);
-        if (std::find(path.begin(), path.end(), pair) != path.end())
-        {
-          std:: cout << " *  ";
-        }
-        else
-        {
-          if ((int)map.data[map.info.width*i + j] > 0)
-          {
-            std::cout << " 1  ";
-          }
-          else if ((int)map.data[map.info.width*i + j] == 0)
-          {
-            std::cout << " 0  ";
-          }
-          else
-          {
-            std::cout << (int)map.data[map.info.width*i + j] << "  ";
-          }
-        }
-      }
-      std::cout << std::endl;
-    }
+    generate_path(path, msg1);
+    // for (int i = 0; i < map.info.height; i++)
+    // {
+    //   for (int j = 0; j < map.info.width; j++)
+    //   {
+    //     std::pair<int, int> pair(i,j);
+    //     if (std::find(path.begin(), path.end(), pair) != path.end())
+    //     {
+    //       std:: cout << " *  ";
+    //     }
+    //     else
+    //     {
+    //       if ((int)map.data[map.info.width*i + j] > 0)
+    //       {
+    //         std::cout << " 1  ";
+    //       }
+    //       else if ((int)map.data[map.info.width*i + j] == 0)
+    //       {
+    //         std::cout << " 0  ";
+    //       }
+    //       else
+    //       {
+    //         std::cout << (int)map.data[map.info.width*i + j] << "  ";
+    //       }
+    //     }
+    //   }
+    //   std::cout << std::endl;
+    // }
   }
   else
   {
@@ -186,7 +232,6 @@ int main(int argc, char **argv)
    * part of the ROS system.
    */
   ros::init(argc, argv, "listener");
-
   /**
    * NodeHandle is the main access point to communications with the ROS system.
    * The first NodeHandle constructed will fully initialize this node, and the last
@@ -211,21 +256,39 @@ int main(int argc, char **argv)
    */
 // %Tag(SUBSCRIBER)%
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::Publisher path_pub = n.advertise<nav_msgs::Path>("/NavfnROS/plan", 1000);
+  ros::Publisher goal_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
+
+
 // %EndTag(SUBSCRIBER)%
+  Listener listener;
+  message_filters::Subscriber<nav_msgs::OccupancyGrid> map_sub(n, "map", 100);
+  message_filters::Subscriber<nav_msgs::Odometry> pos_sub(n, "odom", 100);
+  message_filters::Subscriber<geometry_msgs::PoseStamped> goal_sub(n, "move_base_simple/goal", 100);
+  // message_filters::TimeSynchronizer<nav_msgs::OccupancyGrid, nav_msgs::Odometry> sync(map_sub, pos_sub, 100);
+  typedef message_filters::sync_policies::ApproximateTime<nav_msgs::OccupancyGrid, nav_msgs::Odometry, geometry_msgs::PoseStamped> MySyncPolicy;
+  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(20), map_sub, pos_sub, goal_sub);
+  sync.registerCallback(boost::bind(&Listener::chatterCallbackTurtleBot, &listener, _1, _2, _3));
 
-  message_filters::Subscriber<nav_msgs::OccupancyGrid> map_sub(n, "map", 1);
-  message_filters::Subscriber<nav_msgs::Odometry> pos_sub(n, "odom", 1);
-  message_filters::TimeSynchronizer<nav_msgs::OccupancyGrid, nav_msgs::Odometry> sync(map_sub, pos_sub, 10);
-  sync.registerCallback(boost::bind(&chatterCallbackTurtleBot, _1, _2));
-
+  // std::cout << path.poses[0].pose.position.x << " " << path.poses[0].pose.position.y << std::endl;
+  // std::cout << path.poses[1].pose.position.x << " " << path.poses[1].pose.position.y << std::endl;
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
    * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
-// %Tag(SPIN)%
-  ros::spin();
+// %Tag(SPIN)%  
+  ros::Rate r(10);
+  while (ros::ok())
+  {
+    nav_msgs::Path path = listener.get_path();
+    //geometry_msgs::PoseStamped goal_pose = listener.get_goal();
+    path_pub.publish(path);
+    //goal_pub.publish(goal_pose);
+    ros::spinOnce();
+    r.sleep();
+  }
 // %EndTag(SPIN)%
 
   return 0;
