8bcd7f3a5da014ddc5a66a2a1f93ff4bff36faf9
Chris Erndteman <chrisern@umich.edu>
2023-01-29 17:17:53 -0500

    GPS listener

--- src/gps_listener.cpp
+++ src/gps_listener.cpp
@@ -11,38 +11,31 @@ class GPSdata {
     public:
     gps_common::GPSFix gpsMsg;
 
-    GpsTest(ros::NodeHandle nh_) : n(nh_) {
+    GPSdata(ros::NodeHandle nh_) {
         // Subscribing to the topic /NavSAtFix
-        :gps_sub{n.subscribe("/NavSatFix", 100, &GpsTest::gpsCallback)}
+        gps_sub = nh_.subscribe("/gps/fix", 100, gpsCallback);
     }
 
     // Callback Function for the GPS
-   private:
-    // Nodehandle
-    ros::NodeHandle nh;
+    private:
 
     // Subscriber
     ros::Subscriber gps_sub;
 
     void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
-   
-    // might need to fix this later
-    ROS_INFO("Received Message: ", msg->data.c_str());
-}
+        // might need to fix this later
+        // ROS_INFO("GPS: %f, %f", msg->latitude, msg->longitude);
+        return;
+    }
 };
 
 
 //For constant gps points
-struct LongLatStorage;{
-
-   LongLatStorage(double in_long, double in_lat)
-   :longitude{in_long}, latitude{in_lat}
-
-
+struct LongLatStorage{
+   //LongLatStorage(double in_long, double in_lat)
+   //:longitude{in_long}, latitude{in_lat}
    const double longitude;
    const double latitude;
-
-
 } 
 
 //Requires: given gps format is latitude then longitude
@@ -77,11 +70,9 @@ void set_const_cords(std::vector<LongLatStorage>& given_gps_point){
         else {
             exit(1);
         }
-
-      
-
     }
 }
+/*
 void gps_transform(GPSdata& gps){
    
    
@@ -90,14 +81,9 @@ void gps_transform(GPSdata& gps){
    
    GPSdata.longitude * 111 *  
 
-   GPSdata.latitude 
-   
-
-   
-   
+   //GPSdata.latitude    
 }
-
-
+*/
 
 int main(int argc, char **argv) {
 
@@ -107,33 +93,26 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "gps_listener");
     std::vector<LongLatStorage> given_gps_point;
 
-    GPSdata gps_node;
-
-    gps_node.gps_sub = gps_node.nh.subscribe("/gps/fix", 10, gpsCallback);
 
 
     // Initializing the node for the GPS
-    ros::init(argc, argv, "gps_Subscriber");
-    ros::NodeHandle nh_;
-
-    GpsTest *test_ptr = new GpsTest(nh_);
+    // ros::init(argc, argv, "gps_Subscriber");
+    ros::NodeHandle nh;
+    GPSdata gps_node(nh);
 
     // Getting the data from the GPS
-    
-    
     double gpsLat = 0;
     double gpsLong = 0;
 
     ros::Rate rate(10);
     while(ros::ok()) {
 
-      gpsLong = test_ptr->gpsMsg.longitude;
-      gpsLat = test_ptr->gpsMsg.latitude;
+      gpsLong = gps_node->gpsMsg.longitude;
+      gpsLat = gps_node->gpsMsg.latitude;
 
-      gps_transform(GpsTest);
 
-    //   std::cout << "Current Latitude: " << gpsLat << std::endl;
-    //   std::cout << "Current Longitude " << gpsLong << std::endl;
+    //   gps_transform(gps_node);
+
       ROS_INFO("Current Latitude: %f", gpsLat);
       ROS_INFO("Current Longitude: %f", gpsLong);
 
